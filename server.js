12e551d5ff2244b3edeac5a4fe33aba4ba0b001b
unknown <paulas@Paulas-PC.lar2.mobilestreams.com>
2015-09-25 18:17:18 -0300

    'hoy'

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ router.route('/oauth2/authorize')
   .get(authController.isAuthenticated, oauth2Controller.authorization)
   .post(authController.isAuthenticated, oauth2Controller.decision);
   
-  router.route('/oauth2/token')
+router.route('/oauth2/token')
   .post(authController.isClientAuthenticated, oauth2Controller.token);
 
 // Register all our routes with /api
